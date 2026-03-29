e359baa4fe706c9ef94181ec505734d73c736a7b
Bellika <markusbellika@gmail.com>
2026-03-30 00:24:00 +0200

    added railway to vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['aidev26-fe-production.up.railway.app'],
+  },
+  server: {
+    allowedHosts: ['aidev26-fe-production.up.railway.app'],
+  },
 })
